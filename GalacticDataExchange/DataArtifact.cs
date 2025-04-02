ecb869bd8ce27c019d88d15c99bdec7730c50b2c
Gabriel <gvogt@asu.edu>
2025-04-02 11:13:05 -0700

    Created properties for DataArtifact, an AlienLanguageConverter class and a DataArtifactPage

---
+++ GalacticDataExchange/DataArtifact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDataExchange
+{
+    internal class DataArtifact
+    {
+        [Key]
+        private Guid ID = Guid.NewGuid();
+
+        // Common Properties
+        private string _Name { get; init; }
+
+        private string _ArtifactType { get; init; }
+
+        private string _ImageURL { get; init; }
+
+        private string _RawAlienText { get; init; }
+
+        private string _TranslatedText { get; set; }
+
+        private DateTime _TimeStamp { get; init; }
+
+        private string _SourceLanguage { get; init; }
+
+    }
+}
