5ebcf81874f8054f2a57b5cf24b561996e3dd479
Gabriel <gvogt@asu.edu>
2025-04-02 12:17:03 -0700

    changed DataArtifactPage to have a Select for DataArtifactType. building out DataArtifactDatabase service and DBContext for EF

---
+++ GalacticDataExchange/DataArtifactDatabaseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDataExchange
+{
+    internal class DataArtifactDatabaseService
+    {
+        private readonly DataDBContext _context;
+
+        public DataArtifactDatabaseService(DataDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DataArtifact> InsertDataAsync(DataArtifact dataArtifact)
+        {
+            _context.DataArtifacts.Add(dataArtifact);
+            await _context.SaveChangesAsync();
+            return dataArtifact;
+        }
+    }
+}
---
+++ GalacticDataExchange/DataDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Media;
+using Microsoft.EntityFrameworkCore;
+
+namespace GalacticDataExchange
+{
+    internal class DataDBContext : DbContext
+    {
+        public DbSet<DataArtifact> DataArtifacts { get; set; }
+        public DbSet<DataArtifactType> DataArtifactTypes { get; set; }
+
+        public DataDBContext(DbContextOptions<DataDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
