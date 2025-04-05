c00b6a9284296c87ef8a7fb64d4ddbffb50e76fe
Gabriel <gvogt@asu.edu>
2025-04-05 15:56:09 -0700

    building out Login with authentication and a Session

---
+++ GalacticDataExchange.Shared/LoginInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDataExchange.Shared
+{
+    public class LoginInputModel
+    {
+        private string _email;
+        private string _password;
+
+        public LoginInputModel(string email, string password)
+        {
+            _email = email;
+            _password = password;
+        }
+    }
+}
