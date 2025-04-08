7481eff3b5f0102dc1ff37f434e6ac64eda7661c
Gabriel <gvogt@asu.edu>
2025-04-07 22:53:29 -0700

    Changed signupmodel to not have null references and added a UserDashboard

--- GalacticDataExchange.Shared/UserSignUpModel.cs
+++ GalacticDataExchange.Shared/UserSignUpModel.cs
@@ -10,13 +10,13 @@ namespace GalacticDataExchange.Shared
     public class UserSignUpModel
     {
         [Required(ErrorMessage = "Email is required."), EmailAddress]
-        public string Email;
+        public string Email = String.Empty;
         [Required(ErrorMessage = "Password is required.")]
-        public string Password;
+        public string Password = String.Empty;
         [Required(ErrorMessage = "First name is required.")]
-        public string FirstName;
+        public string FirstName = String.Empty;
         [Required(ErrorMessage = "Last name is required.")]
-        public string LastName;
+        public string LastName = String.Empty;
 
     }
 }
