fe22b2aa8a3754070d6ad4910ce84b675ee17866
Gabriel <gvogt@asu.edu>
2025-04-07 20:40:08 -0700

    Made login page look more professional and changed properties in logininputmodel to required

--- GalacticDataExchange.Shared/LoginInputModel.cs
+++ GalacticDataExchange.Shared/LoginInputModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,10 @@ namespace GalacticDataExchange.Shared
 {
     public class LoginInputModel
     {
-        public string Email;
-        public string Password;
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email = String.Empty;
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password = String.Empty;
 
     }
 }
