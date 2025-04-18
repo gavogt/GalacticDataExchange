bc99c765f7f2a58b4b31f8b0c99de064a41431bf
Gabriel <gvogt@asu.edu>
2025-04-17 22:59:29 -0700

    Added an IUserEngagementService

---
+++ GalacticDataExchange.Shared/IUserEngagementService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDataExchange.Shared
+{
+    internal interface IUserEngagementService
+    {
+        Task<int> LikedAsync(Guid userID, Guid dataArtifactID);
+        Task<int> UnlikedAsync(Guid userID, Guid dataArtifactID);
+        Task<int> GetLikedCountAsync(Guid dataArtifactID);
+        Task<bool> UserLikedAsync(Guid userID, Guid dataArtifactID);
+
+    }
+}
