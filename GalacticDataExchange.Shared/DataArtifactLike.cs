4f8d2ba6dddc219f342c6186f843d8d02a4ca9de
Gabriel <gvogt@asu.edu>
2025-04-17 22:39:28 -0700

    Added a DataArtifactLike class

---
+++ GalacticDataExchange.Shared/DataArtifactLike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDataExchange.Shared
+{
+    public class DataArtifactLike
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        // Common properties
+        public DateTime TimeStamp { get; set; }
+
+        // Foreign Key
+        public Guid DataArtifactID { get; set; }
+        public Guid UserID { get; set; }
+
+        // EF Navigation
+        public DataArtifact? DataArtifact { get; set; }
+        public User? User { get; set; }
+
+
+    }
+}
