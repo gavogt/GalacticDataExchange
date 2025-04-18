754b97d86e32dc66eae3be4339512d0f972edf39
Gabriel <gvogt@asu.edu>
2025-04-17 23:44:13 -0700

    Added a DataArtifactViewModel class

---
+++ GalacticDataExchange.Shared/DataArtifactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticDataExchange.Shared
+{
+    public class DataArtifactViewModel
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public int DataArtifactTypeID { get; init; } 
+        public string? ThumbnailURL { get; init; }
+        public string TranslatedText { get; init; } = string.Empty;
+        public int LikeCount { get; set; } 
+    }
+}
